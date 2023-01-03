e59b8bb7cc65fdefe53429848a02a1bbd5d8ad30
Max16 <max16@naver.com>
2023-01-03 11:54:54 +0900

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,67 @@ import 'package:splash_screen/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Root(),
     ),
   );
+}
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  Color color = Colors.blue;
+  bool show = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: show ? HomeScreen(color: color) : Container(),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        color = color == Colors.red ? Colors.blue : Colors.red;
+                      });
+                    },
+                    child: const Text('색상 변경하기'),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        show = !show;
+                      });
+                    },
+                    child: Text(
+                      !show ? '위젯 생성하기' : '위젯 삭제하기',
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,36 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
-  WebViewController? constroller;
-  final String homeUrl = 'https://blog.codefactory.ai';
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  HomeScreen({super.key, required this.color}) {
+    print('Widget Constructor 실행');
+  }
+
+  @override
+  State<HomeScreen> createState() {
+    print('createState 실행');
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    print('initState 실행');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies 실행');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate 실행');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose 실행');
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget 실행');
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: const Text('Code Factory'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              if (constroller == null) {
-                return;
-              }
-              constroller!.loadUrl(homeUrl);
-            },
-            icon: const Icon(Icons.home),
+    print('build 실행');
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          number++;
+        });
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(
+          child: Text(
+            number.toString(),
           ),
-        ],
-      ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          constroller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
+        ),
       ),
     );
   }
