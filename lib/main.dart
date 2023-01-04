ef29fad9e1d6b6ce009eb94e76a79ffd8f2aa9c8
Max16 <max16@naver.com>
2023-01-04 10:12:37 +0900

    PageView & Timer

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Root(),
+      home: HomeScreen(),
     ),
   );
-}
-
-class Root extends StatefulWidget {
-  const Root({super.key});
-
-  @override
-  State<Root> createState() => _RootState();
-}
-
-class _RootState extends State<Root> {
-  Color color = Colors.blue;
-  bool show = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: show ? HomeScreen(color: color) : Container(),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        color = color == Colors.red ? Colors.blue : Colors.red;
-                      });
-                    },
-                    child: const Text('색상 변경하기'),
-                  ),
-                  const SizedBox(
-                    height: 4.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        show = !show;
-                      });
-                    },
-                    child: Text(
-                      !show ? '위젯 생성하기' : '위젯 삭제하기',
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,71 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
-  final Color color;
-
-  HomeScreen({super.key, required this.color}) {
-    print('Widget Constructor 실행');
-  }
+  const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() {
-    print('createState 실행');
-    return _HomeScreenState();
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int number = 0;
+  Timer? timer;
+  PageController controller = PageController(initialPage: 0);
 
   @override
   void initState() {
-    print('initState 실행');
     super.initState();
-  }
 
-  @override
-  void didChangeDependencies() {
-    print('didChangeDependencies 실행');
-    super.didChangeDependencies();
-  }
+    timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
 
-  @override
-  void deactivate() {
-    print('deactivate 실행');
-    super.deactivate();
+      controller.animateToPage(
+        nextPage,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.linear,
+      );
+    });
   }
 
   @override
   void dispose() {
-    print('dispose 실행');
+    controller.dispose();
+    if (timer != null) {
+      timer!.cancel();
+    }
     super.dispose();
   }
 
   @override
-  void didUpdateWidget(covariant HomeScreen oldWidget) {
-    print('didUpdateWidget 실행');
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print('build 실행');
-
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          number++;
-        });
-      },
-      child: Container(
-        width: 50.0,
-        height: 50.0,
-        color: widget.color,
-        child: Center(
-          child: Text(
-            number.toString(),
-          ),
-        ),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: [1, 2, 3, 4, 5]
+            .map((e) => Image.asset(
+                  'asset/img/image_$e.jpeg',
+                  fit: BoxFit.cover,
+                ))
+            .toList(),
       ),
     );
   }
